629a782583f657631336f0cf1b07fe05c403795f
Jimena-chan <jiimenagramajo@gmail.com>
2020-11-21 23:17:51 -0600

    Day 1: drum-kit version

---
+++ Drum-Kit/js/index.js
@@ -0,0 +1,21 @@
+function playKey(sound) {
+    const audio = document.querySelector(`audio[data-key="${sound.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${sound.keyCode}"]`);
+
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+
+}
+
+function removeTransi(pressSound) {
+    if (pressSound.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => {
+    key.addEventListener('transitionend', removeTransi)
+});
+window.addEventListener('keydown', playKey)
