63e5916548298c4f7fab4ed5de36b00f9dc4dd70
Jimena-chan <jiimenagramajo@gmail.com>
2020-12-10 21:54:27 -0600

    day 18, geolocation

--- Geolocation/js/index.js
+++ Geolocation/js/index.js
@@ -3,7 +3,9 @@ const speed = document.querySelector(".speed-value");
 
 navigator.geolocation.watchPosition(
   (data) => {
+    console.log(data);
     speed.textContent = Math.round(data.coords.speed);
+    // console.log(speed.textContent)
     arrow.style.transform = `rotate(${data.coords.heading}deg)`;
   },
   (error) => {
